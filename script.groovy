dcd175731f5c52d86b969c1d4bc019733aba2011
Ghazouani Hichem <98361932+GhazouaniHichem@users.noreply.github.com>
2022-08-27 17:09:37 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -9,7 +9,7 @@ def buildImage() {
 
 def deployApp() {
     echo 'deploying the application...'
-    sh 'ssh -i ~/web-server-key-pair.pem -o "StrictHostKeyChecking no" ec2-user@13.38.63.231 -y "git clone https://github.com/GhazouaniHichem/complete-cicd-app.git temp && mv temp/.git complete-cicd-app/.git && rm -rf temp && cd complete-cicd-app && docker-compose up -d"'
+    sh 'ssh -i ~/web-server-key-pair.pem -o "StrictHostKeyChecking no" ec2-user@13.38.63.231 -y "git clone https://github.com/GhazouaniHichem/complete-cicd-app.git temp && mv -f temp/.git complete-cicd-app/.git && rm -rf temp && cd complete-cicd-app && docker-compose up -d"'
 //    sh 'git clone https://github.com/GhazouaniHichem/complete-cicd-app.git'
 //    sh 'cd complete-cicd-app'
 //    sh 'docker-compose up -d'
