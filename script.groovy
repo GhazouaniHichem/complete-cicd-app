85329adb443ef88d4928bbe8a8e01f6245a2708e
Ghazouani Hichem <98361932+GhazouaniHichem@users.noreply.github.com>
2022-08-27 16:29:16 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -9,7 +9,7 @@ def buildImage() {
 
 def deployApp() {
     echo 'deploying the application...'
-    sh 'ssh -i ~/web-server-key-pair.pem ec2-user@13.38.63.231 -yes "mkdir myapp-folder'
+    sh 'ssh -i ~/web-server-key-pair.pem ec2-user@13.38.63.231 -yes "mkdir myapp-folder"'
  //   sh 'mkdir myapp-folder'
  //   sh 'cd myapp-folder'
  //   sh 'git clone https://github.com/GhazouaniHichem/complete-cicd-app.git'
