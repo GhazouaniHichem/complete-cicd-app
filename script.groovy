98d96125bb746b02ad6ca66ad9ec6fb6046ecfb3
Ghazouani Hichem <98361932+GhazouaniHichem@users.noreply.github.com>
2023-03-19 19:13:54 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,6 +1,6 @@
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'DockerHub_credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'DockerHub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t ghazouanihm/cicd-app:1.1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push ghazouanihm/cicd-app:1.1'
@@ -9,7 +9,7 @@ def buildImage() {
 
 def deployApp() {
     echo 'deploying the application....'
-    sh 'ssh -i ~/web-server-key-pair.pem -o "StrictHostKeyChecking no" ec2-user@13.38.38.61 -y "git clone https://github.com/GhazouaniHichem/complete-cicd-app.git && cd complete-cicd-app && docker-compose up -d"'
+    sh 'ssh -i ~/jenk_master_key.pem -o "StrictHostKeyChecking no" ec2-user@52.47.177.29 -y "git clone https://github.com/GhazouaniHichem/complete-cicd-app.git && cd complete-cicd-app && docker-compose up -d"'
 //    sh 'git clone https://github.com/GhazouaniHichem/complete-cicd-app.git'
 //    sh 'cd complete-cicd-app'
 //    sh 'docker-compose up -d'
